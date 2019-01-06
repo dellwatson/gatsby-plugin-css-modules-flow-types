0930f24d7168f44dc3cefe88ea55fb752d5c97e0
Matija Marohnić <matija.marohnic@gmail.com>
2019-01-06 10:58:28 +0100

    Refactor code to use available info

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,20 +1,29 @@
-const isCssModulesRule = ({ test }) => test.source.includes('\\.module\\.css')
-const isCssRules = ({ oneOf }) =>
-  Array.isArray(oneOf) && oneOf.some(isCssModulesRule)
-const isCssLoader = ({ loader }) => /\bcss-loader\b/.test(loader)
-
 const loader = require.resolve('css-modules-flow-types-loader')
 
-exports.onCreateWebpackConfig = ({ getConfig, actions }) => {
+exports.onCreateWebpackConfig = ({ getConfig, loaders, rules, actions }) => {
   const config = getConfig()
-  const cssModulesRule = config.module.rules
-    .find(isCssRules)
-    .oneOf.find(isCssModulesRule)
+
+  const cssLoader = loaders.css()
+  const plainCssRule = rules.css()
+  const cssModulesRule = rules.cssModules()
+
+  const isCssLoader = ({ loader }) => loader === cssLoader.loader
+  const isPlainCssRule = rule => (rule.test.source = plainCssRule.test.source)
+  const isCssModulesRule = rule =>
+    (rule.test.source = cssModulesRule.test.source)
+  const isAnyCssRule = rule => isPlainCssRule(rule) || isCssModulesRule(rule)
+  const cssRules = config.module.rules.find(
+    rule => Array.isArray(rule.oneOf) && rule.oneOf.every(isAnyCssRule)
+  )
   const cssModulesFlowTypesLoader = {
     loader,
     options: {},
   }
-  const insertIndex = cssModulesRule.use.findIndex(isCssLoader)
-  cssModulesRule.use.splice(insertIndex, 0, cssModulesFlowTypesLoader)
+
+  cssRules.oneOf.filter(isAnyCssRule).forEach(rule => {
+    const insertIndex = rule.use.findIndex(isCssLoader)
+    rule.use.splice(insertIndex, 0, cssModulesFlowTypesLoader)
+  })
+
   actions.replaceWebpackConfig(config)
 }
